7c30ff6d800db48a8ce2a687bac193a6d87820f1
Themba Lucas Ngubeni <thembangubeni04@gmail.com>
2020-04-13 07:52:44 +0200

    Point of sale project initialization.

---
+++ app/Http/Controllers/Api/Authentication/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api\Authentication;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Store a newly created user in storage.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        return response()->json($request);
+    }
+
+    /**
+     * Display the specified user.
+     *
+     * @param User $user
+     * @return JsonResponse
+     */
+    public function show(User $user): JsonResponse
+    {
+        return response()->json($user);
+    }
+
+    /**
+     * Update the specified user in storage.
+     *
+     * @param Request $request
+     * @param User $user
+     * @return JsonResponse
+     */
+    public function update(Request $request, User $user): JsonResponse
+    {
+        return response()->json($user);
+    }
+
+
+    /**
+     * Remove the specified user from storage.
+     *
+     * @param User $user
+     * @return JsonResponse
+     */
+    public function destroy(User $user): JsonResponse
+    {
+        return response()->json($user);
+    }
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('first_name');
+            $table->string('last_surname');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('cellphone_number');
+            $table->string('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
